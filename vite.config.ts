1589b7c3be35302d366715ba6749329a67a4f91e
xiaosi <sam2020888@outlook.com>
2023-08-14 23:10:56 +0800

    fix: 打包base路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path, { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/three-demo',
+  resolve: {
+    alias: {
+      /** @ 符号指向 src 目录 */
+      "@": resolve(__dirname, "./src")
+    }
+  },
 })
